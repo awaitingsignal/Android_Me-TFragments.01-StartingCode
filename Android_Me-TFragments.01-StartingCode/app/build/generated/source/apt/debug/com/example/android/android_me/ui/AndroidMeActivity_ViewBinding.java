3a74f9ba6a7d6a5876549ea9be5e0bd1c204c003
awaitingsignal <awaitingsignal@protonmail.com>
2019-03-07 16:34:45 -0500

    secondary update commit

--- Android_Me-TFragments.01-StartingCode/app/build/generated/source/apt/debug/com/example/android/android_me/ui/AndroidMeActivity_ViewBinding.java
+++ Android_Me-TFragments.01-StartingCode/app/build/generated/source/apt/debug/com/example/android/android_me/ui/AndroidMeActivity_ViewBinding.java
@@ -15,11 +15,11 @@ import java.lang.Override;
 public class AndroidMeActivity_ViewBinding implements Unbinder {
   private AndroidMeActivity target;
 
-  private View view7f07003d;
+  private View view7f070043;
 
-  private View view7f070021;
+  private View view7f070022;
 
-  private View view7f070047;
+  private View view7f07004e;
 
   @UiThread
   public AndroidMeActivity_ViewBinding(AndroidMeActivity target) {
@@ -33,7 +33,7 @@ public class AndroidMeActivity_ViewBinding implements Unbinder {
     View view;
     view = source.findViewById(R.id.head_container);
     if (view != null) {
-      view7f07003d = view;
+      view7f070043 = view;
       view.setOnClickListener(new DebouncingOnClickListener() {
         @Override
         public void doClick(View p0) {
@@ -43,7 +43,7 @@ public class AndroidMeActivity_ViewBinding implements Unbinder {
     }
     view = source.findViewById(R.id.body_container);
     if (view != null) {
-      view7f070021 = view;
+      view7f070022 = view;
       view.setOnClickListener(new DebouncingOnClickListener() {
         @Override
         public void doClick(View p0) {
@@ -53,7 +53,7 @@ public class AndroidMeActivity_ViewBinding implements Unbinder {
     }
     view = source.findViewById(R.id.leg_container);
     if (view != null) {
-      view7f070047 = view;
+      view7f07004e = view;
       view.setOnClickListener(new DebouncingOnClickListener() {
         @Override
         public void doClick(View p0) {
@@ -70,17 +70,17 @@ public class AndroidMeActivity_ViewBinding implements Unbinder {
     target = null;
 
 
-    if (view7f07003d != null) {
-      view7f07003d.setOnClickListener(null);
-      view7f07003d = null;
+    if (view7f070043 != null) {
+      view7f070043.setOnClickListener(null);
+      view7f070043 = null;
     }
-    if (view7f070021 != null) {
-      view7f070021.setOnClickListener(null);
-      view7f070021 = null;
+    if (view7f070022 != null) {
+      view7f070022.setOnClickListener(null);
+      view7f070022 = null;
     }
-    if (view7f070047 != null) {
-      view7f070047.setOnClickListener(null);
-      view7f070047 = null;
+    if (view7f07004e != null) {
+      view7f07004e.setOnClickListener(null);
+      view7f07004e = null;
     }
   }
 }
--- Android_Me-TFragments.01-StartingCode/app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ Android_Me-TFragments.01-StartingCode/app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.Display;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +13,6 @@ import android.widget.ImageView;
 import com.example.android.android_me.R;
 
 import java.util.List;
-
 
 public class BodyPartFragment extends Fragment {
 
@@ -24,23 +23,12 @@ public class BodyPartFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d("BodyPartFragment", "onCreateViewHeadFragment");
         View view = inflater.inflate(R.layout.fragment_body_part, container, false);
         fragmentPartImageView = view.findViewById(R.id.fragment_image);
         if(imageIds != null) {
             fragmentPartImageView.setImageDrawable((getResources().getDrawable(imageIds.get(displayIndex))));
-            fragmentPartImageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(displayIndex<imageIds.size()-1)
-                    {
-                        displayIndex++;
-                    }
-                    else
-                        displayIndex=0;
-                    fragmentPartImageView.setImageResource(imageIds.get(displayIndex));
-                }
-            });
-
+        }
         return view;
     }
 
